6289737c0ca8a8e32c8a3415f4fc980e87ec2b57
const-kalty <anjolaoluwa055@gmail.com>
2023-02-06 09:48:43 +0100

    frontend UI with html,css and vanilla javascript

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express');
 const dotenv = require('dotenv').config({path:"./vars/.env"});
 const port = process.env.PORT ||  5000;
@@ -5,6 +7,7 @@ const port = process.env.PORT ||  5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/openai', require('./route/openaiRoutes.js'))
 
---
+++ public/js/main.js
@@ -0,0 +1,47 @@
+const submitHandler = (e) => {
+  e.preventDefault();
+  document.querySelector('.msg').textContent = "";
+  document.querySelector('#image').src = "";
+  const prompt = document.querySelector("#prompt").value;
+  const size = document.querySelector("#size").value;
+  if (prompt === "") {
+    alert("Input some text please");
+    return;
+  }
+  generateImageRequest(prompt, size);
+};
+const generateImageRequest = async (prompt, size) => {
+  try {
+    showSpinner();
+    const response = await fetch("/openai/generateimage", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+        size,
+      }),
+    });
+    if(!response.ok){
+        removeSpinner();
+        throw new Error("That image could not be generated");
+    }
+    const result = await response.json();
+    console.log(result);
+    const imageUrl = result.data;
+    document.querySelector('#image').src = imageUrl;
+    removeSpinner();
+  } catch (error) {
+     document.querySelector('.msg').textContent = error;
+  }
+};
+
+const showSpinner = () => {
+  document.querySelector(".spinner").classList.add("show");
+};
+const removeSpinner = () => {
+  document.querySelector(".spinner").classList.remove("show");
+};
+
+document.querySelector("#image-form").addEventListener("submit", submitHandler);
